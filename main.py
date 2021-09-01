e3601c39f69dfde27eabaa14c0721b1ab5dfd0ba
Reem57 <benishai.r@gmail.com>
2021-09-01 13:01:23 -0700

    hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
@@ -10,8 +10,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-# connects /kangaroos path to render kangaroos.html
 @app.route('/kangaroos/')
 def kangaroos():
     return render_template("kangaroos.html")
@@ -28,9 +26,18 @@ def hawkers():
 
 
 @app.route('/stub/')
-def stub():
+def Stub():
     return render_template("stub.html")
 
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("greet.html", name=name)
+    # starting and empty input default
+    return render_template("greet.html", name="World")
 
 # runs the application on the development server
 if __name__ == "__main__":
