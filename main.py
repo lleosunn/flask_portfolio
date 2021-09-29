662b0209f8cc0f80402ab188406e0fdd06c1fd2e
Reem57 <benishai.r@gmail.com>
2021-09-29 13:18:26 -0700

    grayscale

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
+from PIL import Image
+from flask import Blueprint, render_template
+from algorithms.image import image_data
 
 # create a Flask instance
 app = Flask(__name__)
@@ -78,7 +81,8 @@ def wireframe():
 
 @app.route('/rgb/')
 def rgb():
-    return render_template("rgb.html")
+    return render_template('rgb.html', images=image_data())
+
 
 # runs the application on the development server
 if __name__ == "__main__":
